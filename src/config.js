438b4bc813dd6c03544cb194818cfb972104d675
wave <986887991@qq.com>
2019-03-11 15:17:34 +0800

    config.js Configuration file optimization

--- src/config.js
+++ src/config.js
@@ -1,8 +1,10 @@
 const IS_DEV = process.env.NODE_ENV !== 'production';
-//Request url
-export const API_ROOT = IS_DEV ? 'http://116.62.135.185:8081/' : 'http://116.62.135.185:8081/';
-//export const API_ROOT = IS_DEV ? 'https://api.nuls.io/' : 'https://api.nuls.io/';
-//Request response time
-export const API_TIME = IS_DEV ? '9000' : '8000';
 //运行环境（true:正式环境，false:测试环境）
 export const RUN_DEV = true;
+//正式、测试网络的api
+const API_URL = RUN_DEV ? 'https://api.nuls.io/' : 'http://116.62.135.185:8081/';
+//Request url
+export const API_ROOT = IS_DEV ? 'http://192.168.1.125:8080/' : API_URL;
+//Request response time
+export const API_TIME = IS_DEV ? '9000' : '8000';
+
