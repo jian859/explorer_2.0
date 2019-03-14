7f6f75efda1cb9f1ab121eadd6470d02ee9c22d7
wave <986887991@qq.com>
2019-03-14 10:57:58 +0800

    修改 nuls查询 bug

--- src/components/BottomBar.vue
+++ src/components/BottomBar.vue
@@ -59,7 +59,7 @@
        * 获取NULS数量信息
        */
       getNULSNumber() {
-        this.$post('/', '', [])
+        this.$post('/', 'getCoinInfo', [])
           .then((response) => {
             //console.log(response);
             if (response.hasOwnProperty("result")) {
@@ -72,7 +72,6 @@
        *  问题反馈 跳转
        **/
       toBugReport() {
-        console.log(RUN_DEV);
         if(RUN_DEV){
           window.open('https://nuls.community/d/135-collect-the-bugs-of-the-mainnet-bugs', '_blank');
         }else {
