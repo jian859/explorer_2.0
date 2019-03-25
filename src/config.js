babaa1343ece3bdee86fed636353b626d3a3ff45
wave <986887991@qq.com>
2019-03-25 15:00:57 +0800

    新增本地连接调试

--- src/config.js
+++ src/config.js
@@ -4,6 +4,6 @@ export const RUN_DEV = true;
 //正式、测试网络的api
 const API_URL = RUN_DEV ? 'https://api.nuls.io/' : 'http://116.62.135.185:8081/';
 //Request url
-export const API_ROOT = IS_DEV ? 'https://api.nuls.io/' : API_URL;
+export const API_ROOT = IS_DEV ? 'http://192.168.1.192:8080' : API_URL;
 //Request response time
 export const API_TIME = IS_DEV ? '9000' : '8000';
